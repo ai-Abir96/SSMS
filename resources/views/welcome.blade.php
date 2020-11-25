d800863e99bdd65ebbac13f1dc73b6324e566efc
Mohammad Azizul Islam Abir <46776496+ai-Abir96@users.noreply.github.com>
2020-11-25 13:28:09 +0600

    ads

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>SSMS</title>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="{{ asset('js/bootstrap.min.js') }}" ></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+                 $(document).on("scroll", onScroll);
+                 //smoothscroll
+                 $('.navbar-nav li a[href^="#"]').on('click', function (e) {
+                     e.preventDefault();
+                     $(document).off("scroll");
+                     $('.navbar-nav li a').each(function () {
+                         $(this).removeClass('active');
+                     })
+                     $(this).addClass('active');
+                     var target = this.hash
+                         , menu = target;
+                     target = $(target);
+                     $('html, body').stop().animate({
+                         'scrollTop': target.offset().top + 2
+                     }, 1000, 'swing', function () {
+                         window.location.hash = target;
+                         $(document).on("scroll", onScroll);
+                     });
+                 });
+             });
+      
+             function onScroll(event) {
+                 var scrollPos = $(document).scrollTop();
+                 $('.navbar-nav li a').each(function () {
+                     var currLink = $(this);
+                     var refElement = $(currLink.attr("href"));
+                     if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                         $('.navbar-nav li a').removeClass("active");
+                         currLink.addClass("active");
+                     }
+                     else {
+                         currLink.removeClass("active");
+                     }
+                 });
+             }
+      
+        </script>
+
     <!-- Latest compiled and minified CSS -->
     
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" >
@@ -81,54 +128,6 @@
 
     </footer>
 
-
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="{{ asset('js/bootstrap.min.js') }}" ></script>
-
-
-  <script type="text/javascript">
-  $(document).ready(function () {
-           $(document).on("scroll", onScroll);
-           //smoothscroll
-           $('.navbar-nav li a[href^="#"]').on('click', function (e) {
-               e.preventDefault();
-               $(document).off("scroll");
-               $('.navbar-nav li a').each(function () {
-                   $(this).removeClass('active');
-               })
-               $(this).addClass('active');
-               var target = this.hash
-                   , menu = target;
-               target = $(target);
-               $('html, body').stop().animate({
-                   'scrollTop': target.offset().top + 2
-               }, 1000, 'swing', function () {
-                   window.location.hash = target;
-                   $(document).on("scroll", onScroll);
-               });
-           });
-       });
-
-       function onScroll(event) {
-           var scrollPos = $(document).scrollTop();
-           $('.navbar-nav li a').each(function () {
-               var currLink = $(this);
-               var refElement = $(currLink.attr("href"));
-               if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-                   $('.navbar-nav li a').removeClass("active");
-                   currLink.addClass("active");
-               }
-               else {
-                   currLink.removeClass("active");
-               }
-           });
-       }
-
-  </script>
 </body>
 
 </html>
